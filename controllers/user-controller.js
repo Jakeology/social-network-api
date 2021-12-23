89b065b8a0dc1ffc898a974ad3535f788501ad15
Jake <xxjakexx14@gmail.com>
2021-12-23 15:21:20 -0500

    removed populate from user model to only show thought ids

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -4,11 +4,6 @@ const userController = {
   // returns all users
   getAllUsers(req, res) {
     User.find({})
-      .populate({
-        path: "thoughts",
-        select: "-__v",
-      })
-      .select("-__v")
       .then((dbUserData) => res.json(dbUserData))
       .catch((err) => {
         console.log(err);
@@ -19,11 +14,6 @@ const userController = {
   // returns one user by id
   getUserById({ params }, res) {
     User.findOne({ _id: params.id })
-      .populate({
-        path: "thoughts",
-        select: "-__v",
-      })
-      .select("-__v")
       .then((dbUserData) => {
         if (!dbUserData) {
           res.status(404).json({ message: "No user found with this id!" });
